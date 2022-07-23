7deba88927d28670feecfdd104b5b9cbf4173c95
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-23 07:25:04 -0500

    added queries and mutations to frontend

--- server/server.js
+++ server/server.js
@@ -2,18 +2,15 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const path = require("path");
-
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-
+const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
-const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
