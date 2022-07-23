58317cf209fd8def0ce930b547ebd8260079ef7a
Robert Jimenez <robertjimenez86@gmail.com>
2022-07-23 12:50:57 -0500

    fixed express.staic error in server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // serve up static assets
 if (process.env.NODE_ENV === "production") {
-  app.use(express.status(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.get("*", (req, res) => {
